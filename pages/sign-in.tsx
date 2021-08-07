6384d4d362a584f8c9b5573c55b505f16ecbcc92
nayyaung9 <naygit.developer@gmail.com>
2021-08-07 18:13:09 +0630

    test: login page validation with react hook form

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -6,17 +6,24 @@ import {
   FormControl,
   Stack,
   Link,
+  FormLabel,
+  Input,
   Heading,
+  FormErrorMessage,
   Text,
   useColorModeValue,
+  Button,
 } from "@chakra-ui/react";
 import { NextPage } from "next";
-import { Formik } from "formik";
-import { InputControl, SubmitButton } from "formik-chakra-ui";
-import { signInValidation } from "utils/form-validation";
+import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { useCurrentUser } from "hooks/index";
 import AuthHeader from "components/header/AuthHeader";
+
+type ILoginPayload = {
+  email: string;
+  password: string;
+};
 
 const SignIn: NextPage = () => {
   const [user, { mutate }] = useCurrentUser();
@@ -27,6 +34,30 @@ const SignIn: NextPage = () => {
     if (user) router.push("/");
   }, [user]);
 
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = async (values: ILoginPayload) => {
+    try {
+      const res = await fetch("/api/auth", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      });
+
+      if (res.status === 201) {
+        const userObj = await res.json();
+        router.push("/");
+        mutate(userObj);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <React.Fragment>
       <AuthHeader />
@@ -36,7 +67,7 @@ const SignIn: NextPage = () => {
         justify={"center"}
         bg={useColorModeValue("gray.50", "gray.800")}
       >
-        <Stack spacing={8} mx={"auto"} maxW={"lg"}  px={6}>
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} px={6}>
           <Stack align={"center"}>
             <Heading fontSize={"4xl"}>Sign in to your account</Heading>
             <Text fontSize={"lg"} color={"gray.600"}>
@@ -44,77 +75,56 @@ const SignIn: NextPage = () => {
               ✌️
             </Text>
           </Stack>
-          <Formik
-            initialValues={{
-              email: "",
-              password: "",
-            }}
-            validationSchema={signInValidation}
-            onSubmit={async (values) => {
-              try {
-                const res = await fetch("/api/auth", {
-                  method: "POST",
-                  headers: { "Content-Type": "application/json" },
-                  body: JSON.stringify(values),
-                });
 
-                if (res.status === 201) {
-                  const userObj = await res.json();
-                  router.push('/')
-                  mutate(userObj);
-                }
-              } catch (error) {
-                console.log(error);
-              }
-            }}
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
           >
-            {({ handleSubmit, values, errors }) => (
-              <Box
-                as="form"
-                rounded={"lg"}
-                bg={useColorModeValue("white", "gray.700")}
-                boxShadow={"lg"}
-                p={8}
-                onSubmit={handleSubmit}
-              >
-                <Stack spacing={4}>
-                  <InputControl
-                    color={"gray.500"}
-                    name="email"
-                    inputProps={{
-                      placeholder: "Email",
-                      background: "gray.100",
-                    }}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Stack spacing={4}>
+                <FormControl isInvalid={errors.email}>
+                  <Input
+                    id="email"
+                    placeholder="Email"
+                    {...register("email", {
+                      required: "Email is required.",
+                    })}
                   />
+                  <FormErrorMessage>
+                    {errors.email && errors.email.message}
+                  </FormErrorMessage>
+                </FormControl>
 
-                  <InputControl
-                    color={"gray.500"}
-                    name="password"
-                    inputProps={{
-                      placeholder: "Password",
-                      background: "gray.100",
-                    }}
+                <FormControl isInvalid={errors.password}>
+                  <Input
+                    id="password"
+                    placeholder="Password"
+                    {...register("password", {
+                      required: "Please enter your password.",
+                      minLength: {
+                        value: 8,
+                        message: "Minimum length should be 8",
+                      },
+                    })}
                   />
-
-                  <Stack spacing={6}>
-                    <SubmitButton
-                      fontFamily={"heading"}
-                      mt={4}
-                      w={"full"}
-                      bgGradient="linear(to-r, red.400,pink.400)"
-                      color={"white"}
-                      _hover={{
-                        bgGradient: "linear(to-r, red.400,pink.400)",
-                        boxShadow: "xl",
-                      }}
-                    >
-                      Sign in
-                    </SubmitButton>
-                  </Stack>
+                  <FormErrorMessage>
+                    {errors.password && errors.password.message}
+                  </FormErrorMessage>
+                </FormControl>
+                <Stack spacing={6}>
+                  <Button
+                    mt={4}
+                    colorScheme="teal"
+                    isLoading={isSubmitting}
+                    type="submit"
+                  >
+                    Submit
+                  </Button>
                 </Stack>
-              </Box>
-            )}
-          </Formik>
+              </Stack>
+            </form>
+          </Box>
         </Stack>
       </Flex>
     </React.Fragment>
