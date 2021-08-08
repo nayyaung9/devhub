67323d4484d45a70d2889d75ede3c2853b9318d4
nayyaung9 <naygit.developer@gmail.com>
2021-08-08 16:11:36 +0630

    feat: add nprogress bar to notice route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,15 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from '../theme';
+import NProgress from "nprogress";
+import Router from "next/router";
 
 import 'styles/globals.css';
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
